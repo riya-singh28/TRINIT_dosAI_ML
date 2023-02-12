e9b1680efbfc6ca00f288f9fd4e8916c11ea846c
ARY2260 <arb.aryan@gmail.com>
2023-02-12 08:22:29 +0530

    bug fix

--- templates/price_wizard.py
+++ templates/price_wizard.py
@@ -4,7 +4,7 @@ import numpy as np
 from utils import list_of_states
 
 price = pd.read_csv("data/price.csv")
-# price = price.astype({'modal_price':'float32'}).dtypes (bug!)
+# price = price.astype({'modal_price':'float32'}).dtypes
 
 def stats_price():
     crops = pd.DataFrame({"Crops we support": ['rice', 'maize', 'chickpea', 'kidneybeans', 'pigeonpeas',
@@ -21,7 +21,7 @@ def stats_price():
         
         price['modal_price'] = price['modal_price'].apply(np.ceil)
         p = price.groupby(['state'])['label','modal_price']
-        pf = p.get_group((state))
+        pf = p.get_group(state)
         vis = pf.sort_values(by='modal_price', ascending=False)
         
         st.bar_chart(vis, x="label", y="modal_price")
